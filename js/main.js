353c9a80c2ed42ee7bd3883a6d7b15d1c0aa1f27
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-17 09:50:49 -0800

    adding environment variable

--- js/main.js
+++ js/main.js
@@ -4,8 +4,31 @@
 // function getDrink(){
 //     let drink = (document.querySelector('input').value)
 
- 
-fetch("https://api.unsplash.com/search/photos?query=wallpapers&client_id=9lQBKPrQjQe082N8nFYs2JVhtdx7-zFfOWlXyPWq2j8")
+const { clear } = require('console');
+const https = require('https')
+
+require('dotenv').config()
+var myAPIkey = process.env.APIkey
+
+// console.log(myAPIkey)
+// console.log(process.env)
+
+// const randomDrinks = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+//   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//   21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+//   31, 32, 33, 34, 35, 37, 38, 39, 40,
+//   42, 43, 44
+// ];
+
+
+// // cycling through the wallpapers:
+// const random = (Math.floor(Math.random() * randomDrinks.length))
+// let drinkList = (randomDrinks[random]);
+// console.log(drinkList)
+
+
+fetch('https://api.unsplash.com/search/photos?query=wallpapers&client_id='+myAPIkey)
   .then(res => res.json()) // parse response as JSON
   .then(data => {
         console.log(data.results)
@@ -21,7 +44,31 @@ fetch("https://api.unsplash.com/search/photos?query=wallpapers&client_id=9lQBKPr
 //hide
 //9lQBKPrQjQe082N8nFYs2JVhtdx7-zFfOWlXyPWq2j8
 
+
+
+// cycling through the characters:
+
+// setInterval (function getDrink(){
+//     const random = (Math.floor(Math.random() * randomDrinks.length))
+//     let drinkList = (randomDrinks[random]);
+//     console.log(drinkList)
+    
+//   fetch(`https://dragonball-api.com/api/characters/${drinkList}`)
+//   .then(res => res.json()) // parse response as JSON
+//   .then(data => {
+//         console.log(data)
+//       document.querySelector('h2').innerText = data.name
+//       document.querySelector('#dbzImg').src = data.image
+//       document.querySelector('h3').innerText = data.description
+//   })
+//   .catch(err => {
+//           console.log(`error ${err}`)
+//   });
+// }, 10000) // changes every 10 seconds
   // https://web.dragonball-api.com/documentation
+
+
+
 
 // const randomDrinks = [
 //   1,
