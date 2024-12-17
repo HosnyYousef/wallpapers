b6408267d1f9b3e56adcc4b8ce91212ac955ffce
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-17 11:22:41 -0800

    trying to get env to work

--- js/main.js
+++ js/main.js
@@ -7,120 +7,121 @@
 // const { clear } = require('console');
 // const https = require('https')
 
-import './loadEnv';
+
 // import express from 'express';
-let myAPIkey = process.env.APIkey
-console.log(myAPIkey);
-require('dotenv').config({path:'.env'})
+// {path:'./config/.env'}
+require("dotenv").config();
+// let myAPIkey = process.env.APIkey
+console.log(process.env.APIkey);
 // var myAPIkey = process.env.APIkey
 
 
-// console.log(myAPIkey)
-// console.log(process.env)
+// // console.log(myAPIkey)
+// // console.log(process.env)
 
-// const randomDrinks = [
-//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-//   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-//   21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-//   31, 32, 33, 34, 35, 37, 38, 39, 40,
-//   42, 43, 44
-// ];
-
-
-// // cycling through the wallpapers:
-// const random = (Math.floor(Math.random() * randomDrinks.length))
-// let drinkList = (randomDrinks[random]);
-// console.log(drinkList)
+// // const randomDrinks = [
+// //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+// //   11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// //   21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+// //   31, 32, 33, 34, 35, 37, 38, 39, 40,
+// //   42, 43, 44
+// // ];
 
 
-fetch('https://api.unsplash.com/search/photos?query=wallpapers&client_id='+myAPIkey)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-        console.log(data.results)
-      // document.querySelector('h2').innerText = data.items[0].name
-      document.querySelector('img').src = data.results[0].urls.full
-      // document.querySelector('h3').innerText = data.items[0].description
-  })
-  .catch(err => {
-          console.log(`error ${err}`)
-  });
+// // // cycling through the wallpapers:
+// // const random = (Math.floor(Math.random() * randomDrinks.length))
+// // let drinkList = (randomDrinks[random]);
+// // console.log(drinkList)
 
 
-//hide
-//9lQBKPrQjQe082N8nFYs2JVhtdx7-zFfOWlXyPWq2j8
-
-
-
-// cycling through the characters:
-
-// setInterval (function getDrink(){
-//     const random = (Math.floor(Math.random() * randomDrinks.length))
-//     let drinkList = (randomDrinks[random]);
-//     console.log(drinkList)
-    
-//   fetch(`https://dragonball-api.com/api/characters/${drinkList}`)
+// fetch('https://api.unsplash.com/search/photos?query=wallpapers&client_id='+myAPIkey)
 //   .then(res => res.json()) // parse response as JSON
 //   .then(data => {
-//         console.log(data)
-//       document.querySelector('h2').innerText = data.name
-//       document.querySelector('#dbzImg').src = data.image
-//       document.querySelector('h3').innerText = data.description
+//         console.log(data.results)
+//       // document.querySelector('h2').innerText = data.items[0].name
+//       document.querySelector('img').src = data.results[0].urls.full
+//       // document.querySelector('h3').innerText = data.items[0].description
 //   })
 //   .catch(err => {
 //           console.log(`error ${err}`)
 //   });
-// }, 10000) // changes every 10 seconds
-  // https://web.dragonball-api.com/documentation
+
+
+// //hide
+// //9lQBKPrQjQe082N8nFYs2JVhtdx7-zFfOWlXyPWq2j8
+
+
+
+// // cycling through the characters:
+
+// // setInterval (function getDrink(){
+// //     const random = (Math.floor(Math.random() * randomDrinks.length))
+// //     let drinkList = (randomDrinks[random]);
+// //     console.log(drinkList)
+    
+// //   fetch(`https://dragonball-api.com/api/characters/${drinkList}`)
+// //   .then(res => res.json()) // parse response as JSON
+// //   .then(data => {
+// //         console.log(data)
+// //       document.querySelector('h2').innerText = data.name
+// //       document.querySelector('#dbzImg').src = data.image
+// //       document.querySelector('h3').innerText = data.description
+// //   })
+// //   .catch(err => {
+// //           console.log(`error ${err}`)
+// //   });
+// // }, 10000) // changes every 10 seconds
+//   // https://web.dragonball-api.com/documentation
 
 
 
 
-// const randomDrinks = [
-//   1,
-//   2,
-//   3,
-//   4,
-//   5,
-//   6,
-//   7,
-//   8,
-//   9,
-//   10
-// ]
+// // const randomDrinks = [
+// //   1,
+// //   2,
+// //   3,
+// //   4,
+// //   5,
+// //   6,
+// //   7,
+// //   8,
+// //   9,
+// //   10
+// // ]
 
-// const random = (Math.floor(Math.random() * randomDrinks.length))
-// let drinkList = (randomDrinks[random]);
-// console.log(drinkList)
+// // const random = (Math.floor(Math.random() * randomDrinks.length))
+// // let drinkList = (randomDrinks[random]);
+// // console.log(drinkList)
 
-// fetch ("https://dragonball-api.com/api/characters"+ drinkList)
-//     .then (res => res.json()) // parse response as JSON
-//     .then (data => {
-//       console.log(data)
-//       document.querySelector('h2').innerText = data.items.name
-//       document.querySelector('img').src = data.items.image
-//       document.querySelector('h3').innerText = data.items.description
-//     })
-//     .catch(err => {
-//       console.log(`error ${err}`)
-//     })
+// // fetch ("https://dragonball-api.com/api/characters"+ drinkList)
+// //     .then (res => res.json()) // parse response as JSON
+// //     .then (data => {
+// //       console.log(data)
+// //       document.querySelector('h2').innerText = data.items.name
+// //       document.querySelector('img').src = data.items.image
+// //       document.querySelector('h3').innerText = data.items.description
+// //     })
+// //     .catch(err => {
+// //       console.log(`error ${err}`)
+// //     })
 
-// // Get Elements
-// setInterval (function getDrink(){
-//   const random = (Math.floor(Math.random() * randomDrinks.length))
-//   let drinkList = (randomDrinks[random]);
-//   console.log(drinkList)
+// // // Get Elements
+// // setInterval (function getDrink(){
+// //   const random = (Math.floor(Math.random() * randomDrinks.length))
+// //   let drinkList = (randomDrinks[random]);
+// //   console.log(drinkList)
 
-//   let apiImages = ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkList)
+// //   let apiImages = ("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + drinkList)
   
-//   fetch(apiImages)
-//   .then(res => res.json()) // parse response as JSON
-//   .then(data => {
-//     console.log(data.drinks)
-//     document.querySelector('h2').innerText = data.drinks[0].strDrink
-//     document.querySelector('img').src = data.drinks[0].strDrinkThumb
-//     document.querySelector('h3').innerText = data.drinks[0].strInstructions
-//   })
-//   .catch(err => {
-//       console.log(`error ${err}`)
-//   });
-// }, 2000) // change every 10 seconds
+// //   fetch(apiImages)
+// //   .then(res => res.json()) // parse response as JSON
+// //   .then(data => {
+// //     console.log(data.drinks)
+// //     document.querySelector('h2').innerText = data.drinks[0].strDrink
+// //     document.querySelector('img').src = data.drinks[0].strDrinkThumb
+// //     document.querySelector('h3').innerText = data.drinks[0].strInstructions
+// //   })
+// //   .catch(err => {
+// //       console.log(`error ${err}`)
+// //   });
+// // }, 2000) // change every 10 seconds
